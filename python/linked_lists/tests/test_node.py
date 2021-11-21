44d98aeedc0def58047bff662a2232fadf29863b
Edchel Stephen <edchelstephens@gmail.com>
2021-11-21 20:06:37 +0800

    update node repr. Ensure value is passed on node creation.

--- python/linked_lists/tests/test_node.py
+++ python/linked_lists/tests/test_node.py
@@ -2,9 +2,10 @@ from python.linked_lists.node import Node
 
 
 def test_node_repr():
+    """Test node repr"""
 
     node = Node(1)
 
-    expected = "Node(value={}, next={})".format(node.value, repr(node.next))
+    expected = "Node(value={}, next -> {})".format(node.value, repr(node.next))
 
     assert expected == repr(node)
