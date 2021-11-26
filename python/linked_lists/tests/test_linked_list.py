28f550d625e539a1cc6b00b470b9bfdc0d35b03e
Edchel Stephen <edchelstephens@gmail.com>
2021-11-26 08:03:06 +0800

    test insert().

--- python/linked_lists/tests/test_linked_list.py
+++ python/linked_lists/tests/test_linked_list.py
@@ -13,6 +13,24 @@ def test_repr():
     assert expected == repr(linked_list)
 
 
+def test_linked_list_creates_linked_nodes_of_items():
+    """LinkedList creates a linked list of nodes of items."""
+
+    items = [1, 2, 3]
+
+    linked_list = LinkedList(*items)
+
+    list_items = []
+
+    head = linked_list.head
+
+    while head is not None:
+        list_items.append(head.value)
+        head = head.next
+
+    assert items == list_items
+
+
 def test_insert_empty_list():
     """Insert item should be added to list."""
     linked_list = LinkedList()
@@ -23,23 +41,6 @@ def test_insert_empty_list():
 
     assert isinstance(linked_list.head, Node)
     assert linked_list.head.value == item
-
-
-def test_is_empty_returns_True_on_empty_list():
-    """is_empty() method returns True on empty list."""
-
-    linked_list = LinkedList()
-
-    assert linked_list.is_empty()
-
-
-def test_is_empty_returns_False_on_non_empty_list():
-    """is_empty() method returns False on non empty list."""
-
-    linked_list = LinkedList()
-    linked_list.insert(1)
-
-    assert not linked_list.is_empty()
 
 
 def test_insert_inserts_item_as_head_on_empty_linked_list():
@@ -63,8 +64,25 @@ def test_insert_successfully_adds_item_on_tail_of_non_empty_linked_list():
     linked_list = LinkedList()
     linked_list.insert(1)
     linked_list.insert(2)
-    
+
     assert linked_list.get_tail().value == item
+
+
+def test_is_empty_returns_True_on_empty_list():
+    """is_empty() method returns True on empty list."""
+
+    linked_list = LinkedList()
+
+    assert linked_list.is_empty()
+
+
+def test_is_empty_returns_False_on_non_empty_list():
+    """is_empty() method returns False on non empty list."""
+
+    linked_list = LinkedList()
+    linked_list.insert(1)
+
+    assert not linked_list.is_empty()
 
 
 def test_get_tail_returns_None_on_empty_list():
