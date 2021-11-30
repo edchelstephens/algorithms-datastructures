18d18363bdfd13a4c1619834635a6903f2f94d19
Edchel Stephen <edchelstephens@gmail.com>
2021-11-30 10:56:21 +0800

    doubly node.

--- python/linked_lists/node.py
+++ python/linked_lists/node.py
@@ -9,3 +9,19 @@ class Node:
     def __repr__(self):
         """Node representation."""
         return "Node(value={}, next -> {})".format(self.value, repr(self.next))
+
+
+class DoublyNode:
+    """A node element containing a value and references to next and previous nodes."""
+
+    def __init__(self, value) -> None:
+        """Initialize node with given value and provide reference to previous and next."""
+        self.value = value
+        self.previous = None
+        self.next = None
+
+    def __repr__(self) -> str:
+        """Doubly node representation."""
+        return """Node(prev -> {}, value={}, next -> {}""".format(
+            self.previous, self.value, self.next
+        )
