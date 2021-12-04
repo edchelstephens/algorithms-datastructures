380a4e24a7f71ec0133101acecbdaa6715cbe209
Edchel Stephen <edchelstephens@gmail.com>
2021-12-04 11:04:13 +0800

    remove node hard code.

--- python/linked_lists/linked_list.py
+++ python/linked_lists/linked_list.py
@@ -4,19 +4,6 @@ from pprint import pprint
 from python.linked_lists.node import Node
 
 
-# class Node:
-#     """A node element containing a value and a reference to the next node."""
-
-#     def __init__(self, value) -> None:
-#         """Initialize node with given value."""
-#         self.value = value
-#         self.next = None
-
-#     def __repr__(self):
-#         """Node representation."""
-#         return "Node(value={}, next -> {})".format(self.value, repr(self.next))
-
-
 class LinkedList:
     """Singly linked list."""
 
