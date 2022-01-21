69c2c3b95d0b4b6f03ab01e18e206b9131bc52f2
Edchel Stephen <edchelstephens@gmail.com>
2022-01-21 08:24:49 +0800

    binary node.

---
+++ python/trees/binary_tree.py
@@ -0,0 +1,18 @@
+from turtle import right
+from typing import Any
+
+
+class BinaryNode:
+    """A binary tree node."""
+
+    def __init__(self, value: Any, left=None, right=None) -> None:
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        return "BinaryNode(value={}, left={}, right={})".format(
+            self.value,
+            id(self.left) if self.left is not None else None,
+            id(self.right) if self.right is not None else None,
+        )
